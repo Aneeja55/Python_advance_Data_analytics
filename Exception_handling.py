7278db40a3111648f5dc74883f194c7e4360d5a3
Aneeja55 <aneeja5505@gmail.com>
2026-02-10 11:32:03 +0530

    classe, exception handling program modified

--- Exception_handling.py
+++ Exception_handling.py
@@ -1,9 +1,12 @@
 try:
-    num=int(input("Enter a number: "))
-    print("number: ", num)
+    name=input("Enter your name: ")
+    age=int(input("Enter your age: "))
+    print(f"Name: {name}, Age: {age}")
 except ValueError:
-    print("Invalid input. Please enter a valid integer.")
+    print("Invalid input. Please enter a valid integer as age.")
+except TypeError as e:
+    print(f"Type error occurred: {e}, please enter a valid input.")
 else:
-    print("You entered a valid number.")
+    print("valid Input, no error.")
 finally:
-    print("Execution completed.")
+    print("Execution completed, Thankyou.")
---
+++ class.py
@@ -0,0 +1,7 @@
+class MyClass:
+    x=5
+    y=10
+p1=MyClass()
+p2=MyClass()
+print(p1.x)
+print(p2.y)
